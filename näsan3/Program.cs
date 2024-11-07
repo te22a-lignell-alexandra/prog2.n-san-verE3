dd3ca261171ce466ca91fbd0b8b4e6eca567429d
Alexandra Lignell <alexandra.lignell@elev.ga.ntig.se>
2024-11-07 11:11:09 +0100

    enkla metoder

--- näsan3/Program.cs
+++ näsan3/Program.cs
@@ -1,5 +1,39 @@
 ﻿
+System.Console.WriteLine(CircleArea(2));
 
-
+Console.ReadLine();
 // -------------------metoder-------------------
+
+static void Hello32()
+{
+    for (int i = 0; i < 32; i++)
+    {
+        System.Console.WriteLine("Hello, World");    
+    }
+}
+
+static int Matte(int number)
+{
+    int newNumber = number*number;
+    return newNumber;
+}
+
+static float Multi(float faktor1, float faktor2)
+{
+    float newNumber = faktor1*faktor2;
+    return newNumber;
+}
+
+static double RightTriangleArea(double b, double h)
+{
+    double area = (b*h)/2;
+    return area;
+}
+
+static double CircleArea(double r)
+{
+    double area = r*r*Math.PI;
+    return area;
+}
+
 
--- näsan3/obj/Debug/net8.0/näsan3.AssemblyInfo.cs
+++ näsan3/obj/Debug/net8.0/näsan3.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("näsan3")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+be32898e14c76ed3e116666834a5b22cbad73d68")]
 [assembly: System.Reflection.AssemblyProductAttribute("näsan3")]
 [assembly: System.Reflection.AssemblyTitleAttribute("näsan3")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
